9743a126e49bb94e343409161f73d8aeabe83da7
DanAugustoMelo <daugustomelo@outlook.com>
2024-06-18 09:52:44 -0300

    Acrescentando a JLBR

--- main_jlaudiobr.py
+++ main_jlaudiobr.py
@@ -34,55 +34,42 @@ if __name__ == "__main__":
         requisicao = requests.get(link)
         site = BeautifulSoup(requisicao.text, "html.parser")
         
-        nome_produto = site.find(class_='productView-title')
-        preco_produto = site.find(class_='price price--withoutTax price-primary')
-        sku_produto = site.find(class_='productView-info-value productView-info-value--sku')
-        image_tag = site.find('img', class_='productView-thumbnail-link is-active')
+        nome_produto = site.find(class_='product-name')
+        
+        preco_elemento = site.find('span', id='variacaoPreco')
+        preco_produto_text = preco_elemento.text.strip() if preco_elemento else None
+        preco_produto = float(preco_produto_text.replace('.', '').replace(',', '.')) if preco_produto_text else None
 
-        # Verificar se a tag de imagem foi encontrada
-        if image_tag:
-            image_link = image_tag.get('data-src', '')  # Usando .get() para evitar erros caso 'data-src' não esteja presente
+        sku_produto = site.find(class_='product-sku ref')
+
+
+        div_thumbnail = site.find('div', class_='thumb')
+        if div_thumbnail:
+            img_tag = div_thumbnail.find('img')
+            if img_tag:
+                image_link = img_tag['src']
+            else:
+                image_link = None
         else:
-            image_link = ''
-        
-        # Tratamento do preço
-        if preco_produto:
-            preco_texto = preco_produto.text
-            if '-' in preco_texto:  # Se houver um intervalo de preços
-                preco_texto = preco_texto.split('-')[1].strip()  # Pegar o segundo valor após o "-"
-            preco_produto = preco_texto
-        else:
-            preco_produto = ''
-        # Verificar se o preço está vazio após o tratamento
-        if preco_produto.strip():
-            # Remover caracteres indesejados e formatar o preço
-            preco_produto = preco_produto.replace('$', '').replace(',', '').replace('.', ',')
-            if ',' in preco_produto and '.' in preco_produto:
-                preco_produto = preco_produto.replace(',', '.').replace('.', ',', 1)
-        
-            # Verificar se a string ainda tem algum conteúdo após a formatação
-            if preco_produto.strip():
-            # Forçar o tipo de dado para número
-                preco_produto = float(preco_produto.replace(',', '.'))
+            image_link = None
 
         # Encontrando a próxima linha vazia na planilha
         proxima_linha = sheet.max_row + 1
         
         # Salvando as informações na planilha na próxima linha vazia
         sheet[f'A{proxima_linha}'] = nome_produto.text if nome_produto else ''
-        sheet[f'B{proxima_linha}'] = preco_produto
+        sheet[f'B{proxima_linha}'] = preco_produto if preco_produto else ''
         sheet[f'C{proxima_linha}'] = sku_produto.text if sku_produto else ''
         sheet[f'D{proxima_linha}'] = image_link
-        sheet[f'E{proxima_linha}'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
         # Adicionando a data na próxima linha vazia
         sheet[f'E{proxima_linha}'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # Lendo os links da planilha Excel
-    planilha = pd.read_excel('03.Input_links_jlaudio.xlsx')
+    planilha = pd.read_excel('03.Input_links_JLaudio_brasil.xlsx')
 
     # Carregando a planilha de produtos existente
-    workbook = openpyxl.load_workbook('02.Output_informacoes_produtos_jlaudio.xlsx')
+    workbook = openpyxl.load_workbook('02.Output_informacoes_produtos_JLaudio_brasil.xlsx')
     sheet = workbook.active
 
     # Verificando se as colunas já existem na planilha
@@ -99,8 +86,8 @@ if __name__ == "__main__":
 
     # Iterando sobre cada linha da planilha de links
     for idx, row in planilha.iterrows():
-        link = row['Links']
+        link = row['Link']
         extrair_informacoes(link, idx + 2)  # Começa da linha 2
         
     # Salvando a planilha
-    workbook.save('02.Output_informacoes_produtos_jlaudio.xlsx')
+    workbook.save('02.Output_informacoes_produtos_JLaudio_brasil.xlsx')
